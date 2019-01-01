f8a0910398177368be9e511c05f3a41c378604e1
Tandongmeng <18984306906@163.com>
2019-01-01 20:02:54 +0800

    First commit.

---
+++ app/src/main/java/com/meng/coolweather/android/gson/Basic.java
@@ -0,0 +1,26 @@
+package com.meng.coolweather.android.gson;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update {
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+
+}
+
+
+
