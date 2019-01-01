bcfc6351631eeea146d4889b2b210d6608602530
Tandongmeng <18984306906@163.com>
2019-01-01 22:12:43 +0800

    改密钥。

--- app/src/main/java/com/meng/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/meng/coolweather/android/WeatherActivity.java
@@ -133,7 +133,7 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据天气id请求城市天气信息。
      */
     public void requestWeather(final String weatherId) {
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=0b0e7314f4394ebf98e44a81cb56e1ef";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
